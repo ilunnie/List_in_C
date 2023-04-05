6bc916bb8087705fcbdf957c49394ec7d45b50f3
Luis Gustavo <67930760+ilunnie@users.noreply.github.com>
2023-04-05 09:41:24 -0300

    Mudando Value para void
    Agora aceita valores tipo int, double e char

--- main.c
+++ main.c
@@ -1,27 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// estrutura de uma lista
+// Tipos aceitados pela lista
+typedef enum {
+    INT,
+    DOUBLE,
+    CHAR,
+    // Adicione outros tipos aqui
+} DataType;
+
+// Estrutura da lista
 typedef struct list {
-    int value;
-    struct list *proximo;
+    void *value;
+    DataType type;
+    struct list *next;
 } List;
 
+
 // Adiciona um novo indice
-List* newIndice(List *lista, int value){
+List* newIndice(List *lista, void *value, DataType type){
     List *novo_elemento = (List*)malloc(sizeof(List));
     novo_elemento->value = value;
-    novo_elemento->proximo = NULL;
+    novo_elemento->type = type;
+    novo_elemento->next = NULL;
     
     if (lista == NULL) {
         return novo_elemento;
     }
     
     List *atual = lista;
-    while (atual->proximo != NULL) {
-        atual = atual->proximo;
+    while (atual->next != NULL) {
+        atual = atual->next;
     }
-    atual->proximo = novo_elemento;
+    atual->next = novo_elemento;
     
     return lista;
 }
@@ -30,34 +41,56 @@ List* newIndice(List *lista, int value){
 void printl(List *lista){
     printf("[");
     while (lista != NULL){
-        printf("%d, ", lista->value);
-        lista = lista->proximo;
+        switch (lista->type) {
+            case INT:
+                printf("%d", *(int*)lista->value);
+                break;
+            case DOUBLE:
+                printf("%lf", *(double*)lista->value);
+                break;
+            case CHAR:
+                printf("%c", *(char*)lista->value);
+                break;
+            // adicione outros tipos aqui
+            default:
+                printf("?");
+                break;
+        }
+        lista = lista->next;
+        if (lista != NULL) {
+            printf(", ");
+        }
     }
     printf("]\n");
 }
 
 // retorna o valor de um indice
-List* getI(List *lista, int indice){
+void* getIndex(List *lista, int indice){
     int i;
     List *atual = lista;
 
     for (i = 0; i < indice; i++) {
-        atual = atual->proximo;
+        atual = atual->next;
     }
 
-    return atual;
+	return atual->value;
 }
 
-// main para testar as funÃ§Ãµes
+// main para testar as funções
 int main(){
     List *teste = NULL;
-
-    teste = newIndice(teste, 10);
-    teste = newIndice(teste, 20);
-    teste = newIndice(teste, 30);
-
+    int a = 10;
+    double b = 20.5;
+    char c = 'a';
+    
+    teste = newIndice(teste, &a, INT);
+    teste = newIndice(teste, &b, DOUBLE);
+    teste = newIndice(teste, &c, CHAR);
+    
     printl(teste);
-    printf("%d\n", getI(teste, 0)->value);
-
+    printf("%d\n", *(int*) getIndex(teste, 0));
+    printf("%lf\n", *(double*) getIndex(teste, 1));
+    printf("%c\n", *(char*) getIndex(teste, 2));
+    
     return 0;
 }
